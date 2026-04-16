82288b51ccff259c92be8d23becd78700d428afa
Alexandr <asenoke5@gmail.com>
2026-04-16 13:01:51 +0300

    Создан файл config.py для получения данных из env

---
+++ config.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    DB_URL: str
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int
+    BUCKET_NAME: str
+    AWS_SECRET_ACCESS_KEY: str
+    AWS_ACCESS_KEY_ID: str
+    ENDPOINT_URL: str
+
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+settings = Settings()
+
+
