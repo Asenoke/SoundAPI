3a1170bff4d93f85f4d0505305866fc472658c23
Alexandr <asenoke5@gmail.com>
2026-04-17 08:18:11 +0300

    Исправлены модели для редактирования, регистрации пользователя

--- api/auth/models.py
+++ api/auth/models.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field, EmailStr, field_validator
 
 
@@ -5,14 +7,34 @@ class UserRegister(BaseModel):
     firstname: str = Field(..., title="First Name")
     lastname: str = Field(..., title="Last Name")
     email: EmailStr = Field(..., title="Email")
+    phone_number: str = Field(..., title="Phone Number")
     password: str = Field(..., title="Password", min_length=8)
+    age: int = Field(..., title="Age", ge=1)
 
     @field_validator("password")
     def password_validator(cls, value: str):
         special_chars = ["/", "!", "@", "#", "$", "%", "^", "&", "*", "+", "-", "?"]
-        for char in special_chars:
-            if char not in value:
-                raise ValueError("В пароле должны быть специальные символы: /!@#$%^&*+-?")
+
+        # Проверяем, есть ли хотя бы один спецсимвол
+        if not any(char in value for char in special_chars):
+            raise ValueError("Пароль должен содержать хотя бы один специальный символ: /!@#$%^&*+-?")
+
+        # Дополнительные проверки (опционально)
+        if not any(c.isupper() for c in value):
+            raise ValueError("Пароль должен содержать хотя бы одну заглавную букву")
+
+        if not any(c.islower() for c in value):
+            raise ValueError("Пароль должен содержать хотя бы одну строчную букву")
+
+        if not any(c.isdigit() for c in value):
+            raise ValueError("Пароль должен содержать хотя бы одну цифру")
+
+        return value
+
+    @field_validator('phone_number')
+    def validate_phone(cls, v: str):
+        if not v.isdigit():
+            raise ValueError('Номер телефона должен содержать только цифры')
 
 
 class UserLogin(BaseModel):
@@ -21,9 +43,10 @@ class UserLogin(BaseModel):
 
 
 class UserEdit(UserRegister):
-    firstname: str = Field(title="First Name")
-    lastname: str = Field(title="Last Name")
-    email: EmailStr = Field(title="Email")
-    password: str = Field(title="Password")
+    firstname: Optional[str] = Field(title="First Name")
+    lastname: Optional[str] = Field(title="Last Name")
+    phone_number: Optional[str] = Field(title="Phone Number")
+    email: Optional[EmailStr] = Field(title="Email")
+    password: Optional[str] = Field(title="Password")
 
 
